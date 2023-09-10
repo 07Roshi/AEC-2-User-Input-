6681d82d885f610b3032c0e51458566f667b1082
Shetty Roshith Raghurama <121103694+07Roshi@users.noreply.github.com>
2023-09-10 18:57:48 +0530

    Update 5.HttpReq.go

--- 5.HttpReq.go
+++ 5.HttpReq.go
@@ -18,30 +18,30 @@ func main() {
 	var postID int
 	fmt.Print("Enter the Post ID: ")
 	fmt.Scan(&postID)
-	// URL to send the GET request
+	
 	url := fmt.Sprintf("https://jsonplaceholder.typicode.com/posts/%d", postID)
-	// Send the GET request
+	
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("HTTP GET request failed: %s\n", err)
 		return
 	}
-	// Ensure the response body is closed after we're done with it
+	
 	defer response.Body.Close()
-	// Read the response body
+	
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Printf("Failed to read response body: %s\n", err)
 		return
 	}
-	// Parse the JSON response into a Post struct
+	
 	var post Post
 	err = json.Unmarshal(body, &post)
 	if err != nil {
 		fmt.Printf("Failed to parse JSON: %s\n", err)
 		return
 	}
-	// Print the parsed post
+	
 	fmt.Printf("UserID: %d\n", post.UserID)
 	fmt.Printf("ID: %d\n", post.ID)
 	fmt.Printf("Title: %s\n", post.Title)
