e06a1f28434ca510e0c8cb44ff4dc102e93102c4
Shetty Roshith Raghurama <121103694+07Roshi@users.noreply.github.com>
2023-09-10 06:59:56 +0000

    update

--- 1b.LargePrimeFact.go
+++ 1b.LargePrimeFact.go
@@ -1,34 +1,21 @@
 package main
+import "fmt"
 
-import (
-	"fmt"
-	"math"
-)
-
-func largestPrimeFactor(n int) int {
-	largest := 1
-
-	for n%2 == 0 {
-		largest = 2
-		n /= 2
-	}
-	for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
-		for n%i == 0 {
-			largest = i
-			n /= i
-		}
-	}
-	if n > 2 {
-		largest = n
-	}
-	return largest
+func LPF(n int) int {
+    var largest int
+    for i:=2;i<=n;i++{
+        for n%i==0{
+            largest =i
+            n/=i
+        }
+    }
+    return largest
 }
 func main() {
-	var number int
-    fmt.Print("Enter a number: ")
-    fmt.Scan(&number)
-	largest := largestPrimeFactor(number)
-	fmt.Println("The largest prime factor of", number, "is", largest)
+    var cap int
+    fmt.Printf("Enter the number: ")
+    fmt.Scan(&cap)
+    fmt.Println("The largest prime factor of",cap, "is:",LPF(cap))
 }
 
 
